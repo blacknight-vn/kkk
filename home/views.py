ac77e00be39e54a057f510deeedcd77d19a1211a
Chí Thành <chithanh@MacBook-Pro-cua-Chi.local>
2022-10-07 19:18:17 +0700

    m

--- home/views.py
+++ home/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
+from .models import Person
 # Create your views here.
 
 
+
 def index(request):
-    return render(request, 'home/index.html')
+    data = {'people': Person.objects.all()}
+    return render(request, 'home/index.html', data)
